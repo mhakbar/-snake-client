9efef0961a12aa10bacdf73beeaa160431c56ee1
mhakbar <habibakbar@live.com>
2022-01-19 15:11:35 -0800

    connect function is moved to client.js file

--- play.js
+++ play.js
@@ -1,31 +1,10 @@
-const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",// IP address here,
-    port: 50541// PORT number here,
-  });
+const connect = require(".//client");//importing connect function from client.js file. 
 
-  // interpret incoming data as text
-  
-  conn.on("connect", () => {
-    console.log("connection successful");// code that does something when the connection is first established
-  });
 
-  conn.on('data', (data) => {
-    console.log(data.toString());
-    conn.end();
-  });
-  
-  
-  
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 
 
 console.log("Connecting ...");
 connect();
+
