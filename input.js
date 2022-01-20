fb7449a90a9b3162596014735e0bdc0c327d89be
mhakbar <habibakbar@live.com>
2022-01-19 17:30:16 -0800

    Update the setupInput() function to accept a single parameter and assign its value to the connection variable.

--- input.js
+++ input.js
@@ -1,6 +1,14 @@
+let connection;
+
+// const setupInput = (conn) => {
+//   connection = conn;
+// };
+
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,8 +21,23 @@ const handleUserInput = function (data) {
   // your code here
   if (data === '\u0003') {
     process.exit();
+} else if (data === 'w') {
+    //console.log('Move: up');
+    connection.write('Move: up');
+} else if (data === 'a') {
+  //console.log('Move: left');
+  connection.write('Move: left');
+} else if (data === 's') {
+  //console.log('Move: down');
+  connection.write('Move: down');
+} else if (data === 'd') {
+  //console.log('Move: right');
+  connection.write('Move: right');
+}
+};
 
-}
-}
+
+
+
 
 module.exports = setupInput;
