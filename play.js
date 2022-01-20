9684b20e2aaab6b2e852af80a354a2b938900cb1
mhakbar <habibakbar@live.com>
2022-01-19 16:10:47 -0800

    handleUserInput created which takes input from keyboard. Able to terminate game using ctrl+C

--- play.js
+++ play.js
@@ -1,10 +1,30 @@
 
 const connect = require(".//client");//importing connect function from client.js file. 
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;  
+};
+
+const handleUserInput = function (data) {
+  // your code here
+  if (data === '\u0003') {
+    process.exit();
+
+}
+}
+
 
 
 
 
 console.log("Connecting ...");
 connect();
+
 
